8fa6b10d129a8213d78009bd64ef1f04b0488dae
teejale <jasmine.leivo95@gmail.com>
2026-04-23 11:55:26 +0200

    add hover styling to button

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -152,7 +152,7 @@ loginBtn.addEventListener("click", () => {
 
 /* ------------------------------- incoming request start here  ------------------------------------------ */
 
-const incomingTab = document.querySelector("#incoming-tab");
+// const incomingTab = document.querySelector("#incoming-tab");
 const statusPara = document.createElement("p");
 statusPara.innerHTML = `Status: `;
 
